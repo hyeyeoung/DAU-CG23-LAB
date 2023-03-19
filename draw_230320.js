14cfbdec8134f8aa37451fb8e550b0502499fa09
Youngjin <yjpark@dau.ac.kr>
2023-03-19 17:18:57 +0900

    230320 실습코드

---
+++ draw_230320.js
@@ -0,0 +1,110 @@
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+
+//Make data
+let linePts = [];
+linePts.push(new THREE.Vector2(50, 50));
+linePts.push(new THREE.Vector2(150, 200));
+
+linePts.push(new THREE.Vector2(50, 250));
+linePts.push(new THREE.Vector2(350, 250));
+
+linePts.push(new THREE.Vector2(50, 50));
+linePts.push(new THREE.Vector2(150, 200));
+
+let boxPts=[];
+
+boxPts.push(new THREE.Vector2(100, 100));
+boxPts.push(new THREE.Vector2(300, 300));
+
+
+function draw_line(p0,p1)
+{
+    ctx.beginPath();
+    ctx.moveTo(p0.x, p0.y);
+    ctx.lineTo(p1.x, p1.y);
+    ctx.stroke();
+}
+
+function draw_point(p)
+{
+    ctx.fillStyle = "#ff0000";
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+function draw_box(minPt,maxPt)
+{
+    let p0 = new THREE.Vector2(minPt.x,minPt.y);
+    let p1 = new THREE.Vector2(minPt.x,maxPt.y);
+    let p2 = new THREE.Vector2(maxPt.x,maxPt.y);
+    let p3 = new THREE.Vector2(maxPt.x,minPt.y);
+    draw_line(p0,p1);
+    draw_line(p1,p2);
+    draw_line(p2,p3);
+    draw_line(p3,p0);
+}
+
+function draw_circle(ctr,radius)
+{
+    ctx.beginPath();
+    ctx.arc(ctr.x, ctr.y, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+ctx.strokeStyle="blue";
+draw_line(linePts[0],linePts[1]);
+ctx.strokeStyle="red";
+draw_line(linePts[2],linePts[3]);
+ctx.strokeStyle="green";
+draw_box(boxPts[0],boxPts[1])
+ctx.strokeStyle="black";
+draw_circle(new THREE.Vector2(230,230),110);
+
+function line_line_intersection(p0, p1, p2, p3) {
+    console.log(p0);
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+
+    // y=ax+b : 직선의 방정식
+    // a:기울기 : y증가량 / x증가량
+    // y=a0x+b0             y=a1x+b1
+    let a0 = (p1.y-p0.y)/(p1.x-p0.x);
+    let b0 = p0.y - a0*p0.x;
+    
+    let a1 = (p3.y-p2.y)/(p3.x-p2.x);
+    let b1 = p2.y - a1*p2.x;
+
+    //직선의 교점? a0x+b0=a1x+b1 --> (a0-a1)x = b1 -b0
+    let intersectionX = (b1-b0)/(a0-a1);
+    let intersectionY = a0*intersectionX+b0;
+
+    if (p0.x > intersectionX || p1.x < intersectionX)
+        return;
+    if (p2.x > intersectionX || p3.x < intersectionX)
+        return;
+    if (p0.y > intersectionY || p1.y < intersectionY)
+        return;
+    if (p2.y > intersectionY || p3.y < intersectionY)
+        return;
+
+    let intersectionPt = new THREE.Vector2(intersectionX, intersectionY);
+    draw_point(intersectionPt);
+}
+
+function line_box_intersection(lineP0,lineP1,boxMinPt,boxMaxPt)
+{
+    //Need to write...
+}
+
+function line_circle_intersection(lineP0,lineP1,circleCtr,circleRadius)
+{
+    //Need to write...
+}
+
+function box_circle_intersection(boxMinPt,boxMaxPt,circleCtr,circleRadius)
+{
+    //Need to write...
+}
